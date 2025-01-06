4f2f47e1ae7ab7abf6438d2cb0ba42c6c760d6ef
lgrosu <lucian.grosu@endava.com>
2025-01-06 18:28:07 +0200

    noutati

--- src/test/java/VariabilaMetoda/Student.java
+++ src/test/java/VariabilaMetoda/Student.java
@@ -1,5 +1,7 @@
 package VariabilaMetoda;
 
+import org.testng.annotations.Test;
+
 public class Student {
 
     // class = sablon care defineste proprietatile si comportamentul unei entitati specifice
@@ -30,8 +32,41 @@ public class Student {
 
     public Float Greutate;
 
-    public  Character Sex;
+    public Character Sex;
 
     public Boolean AreDiploma;
+
+    @Test
+    public void prezentareStudent(){
+        Nume = "Grosu";
+        Prenume = "Lucian";
+        Varsta = 43;
+        Adresa = "Iasi";
+        Inaltime = 1.72;
+        Greutate = 75f;
+        Sex = 'M';
+        AreDiploma = true;
+
+        //System.out.println(Nume + " " + Prenume + "," + " " + Varsta + "," + " " + Adresa + "," + " " + Inaltime +"," + " " + Greutate + "," + " " + Sex + "," + " " + AreDiploma);
+
+        System.out.println("Nume student: " +Nume);
+        System.out.println("Prenume student: " +Prenume);
+        System.out.println("Varsta student: " +Varsta);
+        System.out.println("Adresa student: " +Adresa);
+        System.out.println("Inaltime student: " +Inaltime);
+        System.out.println("Greutate student: " +Greutate);
+        System.out.println("Sex: " +Sex);
+        System.out.println("Studentul are Diploma: " +AreDiploma);
+        calculMedie();
+    }
+
+    public void calculMedie(){
+
+        Integer nota1 = 8;
+        Integer nota2 = 7;
+        Integer nota3 = 9;
+        Integer medie = (nota1+nota2+nota3)/3;
+        System.out.println("Media studentului este: " +medie);
+    }
 
 }
